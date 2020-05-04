7baee201ffde9a9abe565cbeb237d8e6d77232aa
jeky22 <qhzh33@gmail.com>
2020-05-05 02:42:03 +0900

    bugfix#La07 - path에러 메세지 수정

--- src/index.js
+++ src/index.js
@@ -29,7 +29,6 @@ app.use('/search', searchRouter)
 app.use(function (req, res, next) {
   next(createError(404))
 })
-
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
@@ -38,8 +37,9 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500)
-  res.render(err)
+  res.json(err)
   next()
+  
 })
 
 module.exports = app

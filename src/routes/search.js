d301d32673ed15d17dd5e06a40d9e52821a5a9e6
jeky22 <qhzh33@gmail.com>
2020-06-18 18:34:18 +0900

    dev#La10 - make search seller api

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var Seller = require('../models').Seller
 var db = require('../models/index')
+var jwt = require('jsonwebtoken')
 
 var Op = require('sequelize').Op
 
@@ -49,6 +50,60 @@ router.post('/', async (req, res, next) => {
 
       res.json({ success: true, sellerdata: sellers.slice(0, 20) })
     }
+  } catch (err) {
+    console.log(err)
+    res.json({ success: false, error: err })
+  }
+})
+
+router.post('/ent', async (req, res, next) => {
+  try {
+    var token = req.headers['x-access-token']
+    jwt.verify(token, process.env.JWT_KEY, async function (err, decoded) {
+      if (err) return res.json({ success: false, err })
+      console.log(decoded)
+      var lat = decoded.lat
+      var lon = decoded.lon
+      var page = 0
+      var x = '-1.0'
+      var listcount = 20000
+      if (x === '-1.0') {
+        const sellers = await Seller.findAll({
+          raw: true
+        }, { where: { contractable: 0 } })
+
+        sellers.sort((a, b) => {
+          var ax = (lat - a.lat)
+          var ay = (lon - a.lon) * Math.cos(a.lat * Math.PI / 180)
+          var A = 110.25 * Math.sqrt(ax * ax + ay * ay)
+
+          var bx = (lat - b.lat)
+          var by = (lon - b.lon) * Math.cos(b.lat * Math.PI / 180)
+          var B = 110.25 * Math.sqrt(bx * bx + by * by)
+          return A < B ? -1 : 1
+        })
+
+        res.json({ success: true, sellerdata: sellers.slice(listcount * page, listcount * page + listcount) })
+      } else {
+        var range = 293.59 * Math.exp(-0.703 * x)
+
+        const sellers = await Seller.findAll({
+          raw: true,
+          where: {
+            lat: {
+              [Op.gte]: lat - range,
+              [Op.lte]: parseFloat(lat) + range
+            },
+            lon: {
+              [Op.gte]: lon - range,
+              [Op.lte]: parseFloat(lon) + range
+            }
+          }
+        })
+
+        res.json({ success: true, sellerdata: sellers })
+      }
+    })
   } catch (err) {
     console.log(err)
     res.json({ success: false, error: err })
